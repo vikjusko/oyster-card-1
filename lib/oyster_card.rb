df3cb8aefcdb36e481bb3e0df732f1c5225cffa8
Johnny Yip <jyippy2012@gmail.com>
2020-09-07 15:37:10 +0100

    Topup method

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,3 +1,13 @@
 class OysterCard
+
+    attr_reader :balance
+
+    def initialize
+        @balance = 0
+    end
+
+    def top_up(amount)
+        @balance += amount
+    end
+
 end
-    
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,4 +1,12 @@
 require 'oyster_card'
+
 describe OysterCard do
+    it "Balance of oyster card equals 0" do
+        expect(subject.balance).to eq 0
+    end
     
+    it "Top up oyster card with £2" do
+        expect{subject.top_up(2)}.to change{(subject.balance)}.by(2)
+    end 
+
 end    
