9ad5b5caef07022182cac92f75de5cbfd74a1def
Student <student@Makerss-Air.broadband>
2020-09-07 16:13:57 +0100

    Added oyster limit

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,12 +1,12 @@
 class OysterCard
-
-    attr_reader :balance
-
+    attr_reader :balance 
+    OYSTER_LIMIT = 20
     def initialize
         @balance = 0
     end
 
     def top_up(amount)
+        raise 'Maximum Limit' if amount + balance > OYSTER_LIMIT
         @balance += amount
     end
 
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -7,6 +7,12 @@ describe OysterCard do
     
     it "Top up oyster card with £2" do
         expect{subject.top_up(2)}.to change{(subject.balance)}.by(2)
+    end
+    
+    it "Limit for oyster card £90" do
+        limit = OysterCard::OYSTER_LIMIT
+        subject.top_up(limit)  
+       expect{subject.top_up(90) }.to raise_error("Maximum Limit")      
     end 
 
 end    
