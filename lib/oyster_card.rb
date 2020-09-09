ddaba33e4148ceff3522b0bc2751d131e941232c
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-09 16:38:02 +0100

    creating station test spec

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,41 +1,50 @@
 class OysterCard
-    attr_reader :balance, :entry_station, :list_of_journey, :current_journey
-    OYSTER_LIMIT = 90
-    MINIMUM_LIMIT = 1
-    BALANCE = 0
-    def initialize(balance = BALANCE)
-        @balance = balance
-        @entry_station = nil
-        @list_of_journey = []
-        @current_journey = { "entry_station" => nil, "exit_station" => nil }
-    end
+  attr_reader :balance, :entry_station, :list_of_journey, :current_journey
+  OYSTER_LIMIT = 90
+  MINIMUM_LIMIT = 1
+  BALANCE = 0
 
-    def top_up(amount)
-        raise 'Maximum Limit exceeded!' if amount + balance > OYSTER_LIMIT
-        @balance += amount
-    end
+  def initialize(balance = BALANCE)
+    @balance = balance
+    @entry_station = nil
+    @list_of_journey = []
+    @current_journey = { "entry_station" => nil, "exit_station" => nil }
+  end
 
-    def deduct(amount)
-        @balance -= amount
-    end
+  def top_up(amount)
+    raise "Maximum Limit exceeded!" if amount + balance > OYSTER_LIMIT
+    @balance += amount
+  end
 
-    def in_journey?
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
     !!@entry_station
-    end    
+  end
 
-    def touch_in(station)
-     @entry_station = station   
-     @current_journey["entry_station"] = :station
-     fail "No Money" if @balance < MINIMUM_LIMIT    
-    end    
+  def touch_in(entry_station)
+    journey_history
+    @entry_station = entry_station
+    @current_journey["entry_station"] = entry_station
+    fail "No Money" if @balance < MINIMUM_LIMIT
+  end
 
-    def touch_out(station)
-      @entry_station = nil  
-      deduct(MINIMUM_LIMIT)
-    end    
+  def touch_out(exit_station)
+    @entry_station = nil
+    @current_journey["exit_station"] = exit_station
+    deduct(MINIMUM_LIMIT)
+  end
 
-    private :deduct
+  def journey_history
+    if @current_journey["entry_station"] == nil && @current_journey["exit_station"] == nil
+      return
+    else
+      @list_of_journey << @current_journey
+      @current_journey = { "entry_station" => nil, "exit_station" => nil }
+    end
+  end
 
+  private :deduct
 end
-
-
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,52 +1,70 @@
-require 'oyster_card'
+require "oyster_card"
 
 describe OysterCard do
-    let(:station) { double :station }
-    let(:card)    { OysterCard.new(10)}
-    let(:station_2) { double :station }
+  let(:entry_station) { double :station }
+  let(:card) { OysterCard.new(10) }
+  let(:exit_station) { double :station }
 
+  describe "variable states" do
     it "Balance of oyster card equals 0" do
-        expect(subject.balance).to eq 0
+      expect(subject.balance).to eq 0
     end
-    
+
+    it "checks that the list_of_journey is empty by default" do
+      expect(card.list_of_journey).to be_empty
+    end
+  end
+
+  describe "#top_up" do
     it "Top up oyster card with £2" do
-        expect{card.top_up(2)}.to change{(card.balance)}.by(2)
-    end
-    
-    it "Limit for oyster card £90" do 
-       expect{subject.top_up(OysterCard::OYSTER_LIMIT + 1) }.to raise_error("Maximum Limit exceeded!")      
-    end 
-
-    it "Deduct £5 from oyster card" do
-        expect{card.send(:deduct, 5)}.to change{(card.balance)}.from(10).to(5)
+      expect { card.top_up(2) }.to change { (card.balance) }.by(2)
     end
 
+    it "Limit for oyster card £90" do
+      expect { subject.top_up(OysterCard::OYSTER_LIMIT + 1) }.to raise_error("Maximum Limit exceeded!")
+    end
+  end
+
+  it "Deduct £5 from oyster card" do
+    expect { card.send(:deduct, 5) }.to change { (card.balance) }.from(10).to(5)
+  end
+
+  describe "#touch_in" do
     it "Touch in oyster should show true for in_journey?" do
-        card.touch_in(station)
-        expect(card.in_journey?).to be true
+      card.touch_in(entry_station)
+      expect(card.in_journey?).to be true
     end
-
-    it "Touch out oyster should show false for in_journey?" do
-        subject.touch_out(station_2)
-        expect(subject.in_journey?).to be false
-    end    
 
     it "does oyster card show error if there is no balance" do
-        expect{subject.touch_in(station)}.to raise_error"No Money"
+      expect { subject.touch_in(entry_station) }.to raise_error "No Money"
     end
-
-    it 'we have the correct balance after touching out' do 
-      expect {subject.touch_out(station_2)}.to change{(subject.balance)}.by(-(OysterCard::MINIMUM_LIMIT))
-    end 
 
     it "store entry station" do
-        card.touch_in(station)
-    expect(card.entry_station).to eq station
+      card.touch_in(entry_station)
+      expect(card.entry_station).to eq entry_station
+    end
+  end
+
+  describe "#touch_out" do
+    it "Touch out oyster should show false for in_journey?" do
+      subject.touch_out(exit_station)
+      expect(subject.in_journey?).to be false
     end
 
-    it "store entry station at touch in" do
-        card.touch_in(station)
-    expect(card.current_journey).to have_value(:station)
-    end     
+    it "we have the correct balance after touching out" do
+      expect { subject.touch_out(exit_station) }.to change { (subject.balance) }.by(-(OysterCard::MINIMUM_LIMIT))
+    end
+  end
 
-end    
+  describe "adds entry and exit stations to a hash" do
+    it "store entry station at touch in" do
+      card.touch_in(entry_station)
+      expect(card.current_journey).to have_value(entry_station)
+    end
+
+    it "store exit station at touch in" do
+      card.touch_out(exit_station)
+      expect(card.current_journey).to have_value(exit_station)
+    end
+  end
+end
